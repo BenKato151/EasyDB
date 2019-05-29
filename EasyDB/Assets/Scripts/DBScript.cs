3f6b042ee959af4bdf7d6fe2d57a75c30d45048e
BenKato <benjamin.kiel@edu.bib.de>
2019-05-29 18:33:56 +0200

    Added creation of dynamic tables
    Look up TODO.txt

--- EasyDB/Assets/Scripts/DBScript.cs
+++ EasyDB/Assets/Scripts/DBScript.cs
@@ -6,17 +6,26 @@ using System.IO;
 using System;
 using Mono.Data.Sqlite;
 
-public class DBScript : MonoBehaviour {
-
-    public Text dbpath;
+public class DBScript : MonoBehaviour
+{
+    #region Vars
+    public GameObject container;
+    public InputField dbpath;
+    public InputField num_of_rowsField;
+    public InputField table_rows_original;
     public Text consoleMSG;
 
+    private int rows = 0;
+    private GameObject rowscontainer;
+    private InputField[] rowarray;
     private SqliteConnection con;
+    private bool isEntered = false;
+    #endregion
 
     #region Connection
     public void Connect()
-    {
-        Connection(dbpath.text);
+    {        
+        Connection(@"" + dbpath.text);
     }
 
     private void Connection(string database_path)
@@ -25,7 +34,17 @@ public class DBScript : MonoBehaviour {
         {
             if (File.Exists(database_path))
             {
-                if (database_path.EndsWith(".sqlite"))
+                if (num_of_rowsField.text != null)
+                {
+                    CreateTableRows();
+                }
+
+                for (int i = 0; i < database_path.Length; i++)
+                {
+                    database_path.Replace(@"\", "/");
+                }
+
+                if (database_path.EndsWith(".sqlite") && !(rows <=1))
                 {
                     con = new SqliteConnection("Data Source = " + database_path + "; " + " Version = 3;");
                     if (con != null)
@@ -35,21 +54,51 @@ public class DBScript : MonoBehaviour {
                                            database_path;
                     }
                 }
+
                 else
                 {
                     consoleMSG.text = "Database doesn't exists. \n" +
-                                      "Please use the full path to database-file.";
+                                      "You must have atleast more than 1 'Table Rows'";
                 }
             }
             else
             {
-                consoleMSG.text = "Please use a SQLite Database";
+                consoleMSG.text = "Please use the full path to database-file.";
             }
         }
         catch (Exception e)
         {
             Debug.Log("Failed! \n" + e);
             consoleMSG.text = "Failed to connect.";
+        }
+    }
+    #endregion
+
+    #region CreateTableRows
+    private void CreateTableRows()
+    {
+        if (!isEntered)
+        {        
+            rowscontainer = Instantiate(container);
+            rowscontainer.name = "RowsContainer";
+            rowscontainer.transform.SetParent(container.transform);
+
+            Vector3 table_pos = num_of_rowsField.transform.position;
+            rows = int.Parse(num_of_rowsField.text);
+            rowarray = new InputField[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                Quaternion instantiateInWorldSpace = default(Quaternion);
+                InputField tablefields = Instantiate(table_rows_original, new Vector3(table_pos.x,table_pos.y-((i+1)*35), table_pos.z), instantiateInWorldSpace);
+                tablefields.transform.SetParent(rowscontainer.transform);
+                tablefields.GetComponent<InputField>().contentType = InputField.ContentType.Alphanumeric;
+                tablefields.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "table name";
+
+                tablefields.text = "";
+                tablefields.name = "Input_Tabelle_Number_" + (i + 1);
+                rowarray[i] = tablefields;
+            }
+            isEntered = true;
         }
     }
     #endregion
@@ -87,6 +136,8 @@ public class DBScript : MonoBehaviour {
             {
                 con.Close();
                 consoleMSG.text = "Closed connection.";
+                Destroy(rowscontainer);
+                isEntered = false;
             }
         }
         catch (Exception e)
@@ -96,4 +147,5 @@ public class DBScript : MonoBehaviour {
         }
     }
     #endregion
+
 }
