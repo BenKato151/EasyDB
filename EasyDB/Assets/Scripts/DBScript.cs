157fed17b238d94529620eb7f1077efce2684695
BenKato <benjamin.kiel@edu.bib.de>
2019-06-03 13:31:55 +0200

    Edited delete functions
    its not working very well..

--- EasyDB/Assets/Scripts/DBScript.cs
+++ EasyDB/Assets/Scripts/DBScript.cs
@@ -10,16 +10,21 @@ public class DBScript : MonoBehaviour
 {
     #region Vars
     public GameObject container;
+
     public InputField dbpath;
     public InputField num_of_rowsField;
     public InputField table_rows_original;
+    public InputField deleteID;
+    public GameObject deleteContainer;
     public Text consoleMSG;
+    public Text userOutputMSG;
 
     private int rows = 0;
     private GameObject rowscontainer;
     private InputField[] rowarray;
     private SqliteConnection con;
     private bool isEntered = false;
+    private bool isReady = false;
     #endregion
 
     #region Connection
@@ -39,7 +44,7 @@ public class DBScript : MonoBehaviour
                     database_path.Replace(@"\", "/");
                 }
 
-                if (database_path.EndsWith(".sqlite") && !(rows < 1))
+                if (database_path.EndsWith(".sqlite") && !(rows <= 0))
                 {
                     con = new SqliteConnection("Data Source = " + database_path + "; " + " Version = 3;");
                     if (con != null)
@@ -79,29 +84,38 @@ public class DBScript : MonoBehaviour
     }
     private void CreateTableRows()
     {
-        if (!isEntered)
-        {        
-            rowscontainer = Instantiate(container);
-            rowscontainer.name = "RowsContainer";
-            rowscontainer.transform.SetParent(container.transform);
+        try
+        {
+            if (!isEntered)
+            {        
+                rowscontainer = Instantiate(container);
+                rowscontainer.name = "RowsContainer";
+                rowscontainer.transform.SetParent(container.transform);
 
-            Vector3 table_pos = num_of_rowsField.transform.position;
-            rows = int.Parse(num_of_rowsField.text);
-            rowarray = new InputField[rows];
+                Vector3 table_pos = num_of_rowsField.transform.position;
+                rows = int.Parse(num_of_rowsField.text);
+                rowarray = new InputField[rows];
 
-            for (int i = 0; i < rows; i++)
-            {
-                Quaternion instantiateInWorldSpace = default(Quaternion);
-                InputField tablefields = Instantiate(table_rows_original, new Vector3(table_pos.x,table_pos.y-((i+1)*35), table_pos.z), instantiateInWorldSpace);
-                tablefields.transform.SetParent(rowscontainer.transform);
-                tablefields.GetComponent<InputField>().contentType = InputField.ContentType.Custom;
-                tablefields.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "table name";
+                for (int i = 0; i < rows; i++)
+                {
+                    Quaternion instantiateInWorldSpace = default(Quaternion);
+                    InputField tablefields = Instantiate(table_rows_original, new Vector3(table_pos.x,table_pos.y-((i+1)*35), table_pos.z), instantiateInWorldSpace);
+                    tablefields.transform.SetParent(rowscontainer.transform);
+                    tablefields.GetComponent<InputField>().contentType = InputField.ContentType.Custom;
+                    tablefields.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "table name";
 
-                tablefields.text = "";
-                tablefields.name = "Input_Tabelle_Number_" + (i + 1);
-                rowarray[i] = tablefields;
+                    tablefields.text = "";
+                    tablefields.name = "Input_Tabelle_Number_" + (i + 1);
+                    rowarray[i] = tablefields;
+                }
+                isEntered = true;
+                consoleMSG.text = "";
             }
-            isEntered = true;
+        }
+        catch (Exception e)
+        {
+            consoleMSG.text = "You must have atleast one table!";
+            Debug.LogError("Failed!\n " + e);
         }
     }
     #endregion
@@ -116,7 +130,7 @@ public class DBScript : MonoBehaviour
         try
         {
             for (int b = 0; b < rowarray.Length; b++)
-            {            
+            {                
                 string selecting = "SELECT * FROM " + rowarray[b].text.ToString() + " ;";
                 SqliteCommand command = new SqliteCommand(selecting, Scon);
                 SqliteDataReader output = command.ExecuteReader();
@@ -128,11 +142,12 @@ public class DBScript : MonoBehaviour
                         string column_value = output[i].ToString();
                         string table = rowarray[b].text.ToString();
                         string column = output.GetName(i).ToString();
-                        Debug.Log("In der Tabelle " + table + 
-                                  " ist der Wert der Column " + column + " : " + column_value
-                        );
+                        string outputtext = "In der Tabelle " + table + 
+                                  " ist der Wert der Column " + column + " : " + column_value;
+
+                        userOutputMSG.text = userOutputMSG.text + outputtext + "\n \n";
                     }
-                }
+                }                
             }
             consoleMSG.text = "Success! ";
         }
@@ -147,31 +162,51 @@ public class DBScript : MonoBehaviour
     //TODO: Add everything that is explained in comments. Look Up: TODO.txt
 
     #region Delete Values
+
+    //its not working very well
     public void DeleteValuesInnerDB()
     {
         Delete(con);
     }
 
+    public void SetUp()
+    {
+        Isset(isReady);
+    }
+
     private void Delete(SqliteConnection Scon)
     {
-        try
+        if (Isset(isReady))
         {
-            string deleteColumn = " DELETE FROM " + //TextField for deleting column 
-                                      " WHERE ID = @id";
+            try
+            {
+                for (int i = 0; i < rowarray.Length; i++)
+                {
+                    string deletion = "DELETE FROM " + rowarray[i].text.ToString() + " WHERE ID = @id;";
+                    SqliteCommand command = new SqliteCommand(deletion, Scon);
+                    command.Parameters.Add("@id", System.Data.DbType.String).Value = deleteID.text.ToString();
 
-            SqliteCommand Command = new SqliteCommand(deleteColumn, Scon);
-            Command.Parameters.Add("@id", System.Data.DbType.Int32).Value = 0 /* TextField for DB-ID */;
-
-            Command.ExecuteNonQuery();
-            Command.Parameters.Clear();
-            consoleMSG.text = "Deleted Row/s successfully!";
-        }
-        catch (Exception e)
-        {
-            consoleMSG.text = "Failed to delete Values";
-            Debug.LogError("Failed!\n" + e);
+                    command.ExecuteNonQuery();
+                    command.Parameters.Clear();
+                    consoleMSG.text = "Successfully deleted values with ID: " + deleteID.text;
+                }
+            }
+            catch (Exception e)
+            {
+                consoleMSG.text = "Failed to delete Values";
+                Debug.LogError("Failed!\n" + e);
+            }
         }
     }
+
+    private bool Isset(bool isit)
+    {
+        isit = true;
+        deleteContainer.SetActive(true);
+        consoleMSG.text = "ready";
+        return isit;
+    }
+    
     #endregion
 
     #region Update Values
@@ -249,6 +284,9 @@ public class DBScript : MonoBehaviour
             {
                 con.Close();
                 consoleMSG.text = "Closed connection.";
+                userOutputMSG.text = "";
+                dbpath.text = "";
+                num_of_rowsField.text = "";
                 Destroy(rowscontainer);
                 isEntered = false;
             }
